13fd7102de9c49291769b2d21f0f8eb4641cbe76
Andreas Früchtl <andreas.fruechtl@mailbox.org>
2024-08-21 23:36:50 +0200

    update install scripts

--- ec2/aprs-listener/install.sh
+++ ec2/aprs-listener/install.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+# Install necessary packages
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install -y python3-venv
+
 # Install the required Python packages
-pip3 install -r requirements.txt
+python3 -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
 
 # Copy the systemd service file to the appropriate location
 sudo cp aprs-listener.service /etc/systemd/system/
